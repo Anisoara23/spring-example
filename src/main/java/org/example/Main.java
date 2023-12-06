f95f814861fc2a1fd5f45357352a623ab7172ec8
Anisoara Vlasi <anisoara.vlasi@iongroup.com>
2023-12-06 11:45:13 +0200

    Run jobs

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,34 @@
 package org.example;
 
-import org.example.ui.UserInterface;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("configuration/config.xml");
+        Job importBankFromCsvToDbJob = context.getBean("importBankFromCsvToDbJob", Job.class);
+        Job importCustomerFromCsvToDbJob = context.getBean("importCustomerFromCsvToDbJob", Job.class);
+        Job importCustomerFromDbToCsvJob = context.getBean("importCustomerFromDbToCsvJob", Job.class);
 
-        UserInterface userInterface = context.getBean(UserInterface.class);
-        userInterface.displayUserInterface();
+        JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
+
+        try {
+            JobExecution importBankFromCsvToDbJobExecution = jobLauncher.run(importBankFromCsvToDbJob, new JobParameters());
+            System.out.println(importBankFromCsvToDbJobExecution.getStatus());
+
+            JobExecution importCustomerFromCsvToDbJobExecution = jobLauncher.run(importCustomerFromCsvToDbJob, new JobParameters());
+            System.out.println(importCustomerFromCsvToDbJobExecution.getStatus());
+
+            JobExecution importCustomerFromDbToCsvJobExecution = jobLauncher.run(importCustomerFromDbToCsvJob, new JobParameters());
+            System.out.println(importCustomerFromDbToCsvJobExecution.getStatus());
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+
+        context.close();
     }
 }
